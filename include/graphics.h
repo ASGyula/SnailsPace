d972697006163baebacfe76ee3687e74ce76c026
asgyu <hejjasgyula2004@gmail.com>
2026-02-26 22:02:31 +0100

    [MÓDOSÍTVA] camera.h -> WASD irányítás
    [ÚJ] Új grafikai megjelenítési mód
    
    
    Former-commit-id: e8f178a4abe51b0ac08cb86ed7a0550da9c7acc9

--- include/graphics.h
+++ include/graphics.h
@@ -4,10 +4,22 @@
 
 #ifndef SNAILSPACE_GRAPHICS_H
 #define SNAILSPACE_GRAPHICS_H
+#include <stddef.h>
+
 #include "obj_loader.h"
+
+typedef struct{
+    float x, y, z;
+    float r, g, b;
+} PointData;
 
 void setup_projection(int width, int height);
 
 void render_lidar(const Vertex* vertices, int number_of_vertices, float x, float y, float z);
+
+void prepare_lidar_data(Vertex* vertices, int number_of_vertices);
+
+void render_lidar_fast(int number_of_vertices);
+
 
 #endif //SNAILSPACE_GRAPHICS_H
--- src/camera.c
+++ src/camera.c
@@ -7,6 +7,8 @@
 #include <SDL_opengl.h>
 #include <stdio.h>
 
+#define BASE_MOVEMENT_SPEED 5.0f
+
 void initialize_camera(Camera* camera){
     camera->x = 0.0f;
     camera->y = 1.6f;
@@ -14,15 +16,13 @@ void initialize_camera(Camera* camera){
     camera->yaw = 0.0f;
     camera->pitch = 0.0f;
     camera->mouseSpeed = 0.1f;
-    camera->moveSpeed = 0.05f;
+    camera->moveSpeed = 1.0f;
     camera->isInvertedMouseY = true;
 
     SDL_SetRelativeMouseMode(SDL_TRUE);
 }
 
-void handle_camera_input(SDL_Event* event, Camera* camera, bool* isRunning) {
-    if(event->type == SDL_QUIT)*isRunning = false;
-
+void handle_camera_input(SDL_Event* event, Camera* camera){
     if(event->type == SDL_MOUSEMOTION){
         camera->yaw += event->motion.xrel * camera->mouseSpeed;
         if(camera->isInvertedMouseY) {
@@ -34,34 +34,36 @@ void handle_camera_input(SDL_Event* event, Camera* camera, bool* isRunning) {
         if(camera->pitch > 89.0f)camera->pitch=89.0f;
         if(camera->pitch < -89.0f)camera->pitch=-89.0f;
     }
-    
-    if(event->type == SDL_KEYDOWN){
-        float rad_yaw = camera->yaw * (M_PI/180.0f);
+}
 
-        switch(event->key.keysym.sym){
-            case SDLK_w:
-                camera->x += sinf(rad_yaw) * camera->moveSpeed;
-                camera->z -= cosf(rad_yaw) * camera->moveSpeed;
-                break;
-            case SDLK_s:
-                camera->x -= sinf(rad_yaw) * camera->moveSpeed;
-                camera->z += cosf(rad_yaw) * camera->moveSpeed;
-                break;
-            case SDLK_a:
-                camera->x -= cosf(rad_yaw) * camera->moveSpeed;
-                camera->z -= sinf(rad_yaw) * camera->moveSpeed;
-                break;
-            case SDLK_d:
-                camera->x += cosf(rad_yaw) * camera->moveSpeed;
-                camera->z += sinf(rad_yaw) * camera->moveSpeed;
-                break;
-            case SDLK_ESCAPE:
-                *isRunning = false;
-                break;
-            default: break;
-        }
+void handle_wasd_input(SDL_Event* event, Camera* camera, bool* isRunning, float deltaTime){
+    if(event->type == SDL_QUIT)*isRunning = false;
+
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+    float rad_yaw = camera->yaw * (M_PI/180.0f);
+
+    float moveSpeed = camera->moveSpeed * deltaTime * BASE_MOVEMENT_SPEED;
+
+    if(state[SDL_SCANCODE_W]){
+        camera->x += sinf(rad_yaw) * moveSpeed;
+        camera->z -= cosf(rad_yaw) * moveSpeed;
+    }
+    if(state[SDL_SCANCODE_A]){
+        camera->x -= cosf(rad_yaw) * moveSpeed;
+        camera->z -= sinf(rad_yaw) * moveSpeed;
+    }
+    if(state[SDL_SCANCODE_S]){
+        camera->x -= sinf(rad_yaw) * moveSpeed;
+        camera->z += cosf(rad_yaw) * moveSpeed;
+    }
+    if(state[SDL_SCANCODE_D]){
+        camera->x += cosf(rad_yaw) * moveSpeed;
+        camera->z += sinf(rad_yaw) * moveSpeed;
     }
 
+    if(state[SDL_SCANCODE_ESCAPE]){
+        *isRunning = false;
+    }
 }
 
 void update_camera(Camera* camera){
